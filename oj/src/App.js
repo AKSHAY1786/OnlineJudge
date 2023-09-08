4fb2c305783b8d5180093ac61dc678423ae1e5c3
akshay kapoor <kapoorakshay112@gmail.com>
2023-09-08 14:27:48 +0530

    register and login screen

--- oj/src/App.js
+++ oj/src/App.js
@@ -1,9 +1,18 @@
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import TopBar from "./components/topbar/TopBar";
+import Register from "./components/register/Register";
+
+ import  Login from"./components/login/Login"
 function App() {
   return (
 <BrowserRouter>
 <TopBar/>
+
+<Routes>
+<Route path="/register" element={<Register/>}/>
+<Route path="/login" element={<Login/>}/>
+</Routes>
+
 </BrowserRouter>
       );
 }
---
+++ oj/src/components/register/Register.jsx
@@ -0,0 +1,12 @@
+function Register()
+{
+return (
+<div className="register">
+    <span className="registerTitle">Register</span>
+</div>
+
+
+)
+}
+
+export default Register;
